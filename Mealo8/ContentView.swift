056596352f1113a8013013e8a9b40e67897b9985
R <leorahaf@hotmail.com>
2026-06-03 16:19:09 +0300

    Initial Commit

---
+++ Mealo8/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mealo8
+//
+//  Created by Rahaf on 03/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mealo8/Mealo8App.swift
@@ -0,0 +1,17 @@
+//
+//  Mealo8App.swift
+//  Mealo8
+//
+//  Created by Rahaf on 03/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Mealo8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
